f7061f7d990c10b2470e89d202f8145f015dd7ab
kbbgll <kgwack@gmail.com>
2018-10-14 04:29:32 +0300

    first commit

---
+++ ldap_anr_user_filter_loadtime_test.ps1
@@ -0,0 +1,10 @@
+﻿$term = "K"
+$anrfilter = "(&(objectcategory=person)(|(anr="+ $term + ")(mail=" + $term + "*)))"
+$userfilter = "(&(objectcategory=person)(|(sn="+ $term + ")(cn="+ $term + ")(userPrincipalName="+ $term + ")(givenName="+ $term + ")(mail=" + $term + "*)))"
+
+Write-Host "Username, email, firstname/lastname results:"
+Measure-Command {Get-ADUser -LDAPFilter $userfilter } | Select-Object "TotalSeconds"
+
+
+Write-Host "ANR results:"
+Measure-Command {Get-ADUser -LDAPFilter $anrfilter } | Select-Object "TotalSeconds"
